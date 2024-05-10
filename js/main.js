e94e264aff4c182ed2f651e0f7581e749688d913
Nurussafa <165579625+nsafa1979@users.noreply.github.com>
2024-05-10 22:15:54 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,60 @@
+/* ===== Toggle icon navbar ===== */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+/* =====Scrool Section Active Link===== */
+let sections =  document.querySelectorAll('section');
+let navLink = document.querySelectorAll('header nav');
+
+window.onscroll = ()=>{
+    sections.forEach(sec=>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height =  sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top>=offset && top < offset + height){
+            navLink.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + ']').classList.add('active')
+            });
+        };
+    });
+
+
+
+    /* =====Sticky navbar===== */
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky',window.scrollY > 100);
+
+    /*===== remove toggle icon an navbar when click navbar link (scroll) =====*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+/* =====Scroll Reveal===== */
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+})
+
+ScrollReveal().reveal('.home-content, .heading',{origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form',{origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img',{origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content',{origin: 'right'});
+
+/* ===== Typed JS ===== */
+const typed = new Typed('.multiple-text',{
+    strings: ['Web Designer', 'Web Developer', 'WordPress Expert'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    BackDelay:1000,
+    loop: true
+});
